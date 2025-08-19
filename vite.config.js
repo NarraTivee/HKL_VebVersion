cf347678415a782223ed30f44eecc66971cfb035
ukhhaaan.? <163325718+NarraTivee@users.noreply.github.com>
2025-08-20 00:37:51 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/HKL_VebVersion",
+  base: "HKL_VebVersion",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, "./src"),
